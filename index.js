36bbd74e06e2d9031d33cdde0f1ea5d4b23f10a9
ShiroYaksh90 <aljazaeribasim@gmail.com>
2025-12-30 08:09:07 +0300

    Add route for city id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import { dirname } from "path"
 import { fileURLToPath } from "url";
 import cors from "cors"
-
+import citiesData from "./data/cities.json" with { type: 'json' }
 const app = express();
 app.use(cors())
 app.use(express.static("public"));
@@ -19,8 +19,16 @@ app.get('/questions', (req, res) => {
     res.sendFile(__dirname + "/data/questions.json")
 })
 
+const { cities } = citiesData
+
 app.get("/cities", (req, res) => {
     res.sendFile(__dirname + "/data/cities.json")
+})
+
+app.get("/cities/:id", (req, res) => {
+    const cityId = req.params.id
+    const city = cities.filter(city => city.id == cityId)
+    res.json({ city })
 })
 
 app.listen(port, () => {
